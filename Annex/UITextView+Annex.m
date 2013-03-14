af59f5dc7d9017f745ef1a894b77b6ac717438af
Wess Cope <wcope@me.com>
2013-03-14 12:04:01 -0400

    Adds UITextView for visible text range, number of lines

--- Annex/UITextView+Annex.m
+++ Annex/UITextView+Annex.m
@@ -9,6 +9,28 @@
 #import "UITextView+Annex.h"
 
 @implementation UITextView (Annex)
+@dynamic visibleTextRange;
+@dynamic numberOfLines;
+
+- (NSRange)visibleTextRange
+{
+    CGRect bounds           = self.bounds;
+    NSString *text          = self.text;
+    CGSize textSize         = [text sizeWithFont:self.font constrainedToSize:CGSizeMake(bounds.size.width, bounds.size.height)];
+    
+    UITextPosition *start   = [self characterRangeAtPoint:bounds.origin].start;
+    UITextPosition *end     = [self closestPositionToPoint:CGPointMake(textSize.width, textSize.height)];
+    NSUInteger startPoint   = [self offsetFromPosition:self.beginningOfDocument toPosition:start];
+    NSUInteger endPoint     = [self offsetFromPosition:start toPosition:end];
+    
+    return NSMakeRange(startPoint, endPoint);
+}
+
+- (NSUInteger)numberOfLines
+{
+    return (self.contentSize.height / self.font.lineHeight);
+}
+
 - (NSString *)textAtPoint:(CGPoint)point
 {
     UITextPosition *textPosition    = [self closestPositionToPoint:point];
