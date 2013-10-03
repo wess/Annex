3e9a1ec1ecfda4daa91c09361d5c51f64f0ae725
Wess Cope <wcope@me.com>
2013-10-03 16:28:01 -0400

    Adds NSObject selectors that allow for passing specific queues

--- Annex/NSObject+Annex.h
+++ Annex/NSObject+Annex.h
@@ -15,6 +15,24 @@
  */
 
 /**
+ Executes a given block, followed by a callback, with a weak reference to the calling object;
+ using a provided Queue.
+ 
+ @param block       The code to be excuted
+ @param callback    When the block is complete, the code that will be excuted.
+ */
+- (void)executeBlock:(void(^)(__weak id this))block queue:(dispatch_queue_t)queue withCallback:(void(^)(__weak id this))callback;
+
+/**
+ Executes a given block, followed by a callback;
+ using a provided Queue.
+ 
+ @param block       The code to be excuted
+ @param callback    When the block is complete, the code that will be excuted.
+ */
++ (void)executeBlock:(void(^)())block queue:(dispatch_queue_t)queue withCallback:(void(^)())callback;
+
+/**
  Executes a given block, followed by a callback, with a weak reference to the calling object.
  
  @param block       The code to be excuted
@@ -29,6 +47,23 @@
  @param callback    When the block is complete, the code that will be excuted.
  */
 + (void)executeBlock:(void(^)())block withCallback:(void(^)())callback;
+
+/**
+ Executes a given block after a specificed delay, with a weak reference to the calling object;
+ on the provided Queue.
+
+ @param block   The code to be excuted
+ @param delay   How long to wait before excuting block.
+ */
+- (void)executeBlock:(void(^)(__weak id this))block  queue:(dispatch_queue_t)queue afterDelay:(NSTimeInterval)delay;
+
+/**
+ Executes a given block after a specificed delay;  on the provided Queue.
+
+ @param block   The code to be excuted
+ @param delay   How long to wait before excuting block.
+ */
++ (void)executeBlock:(void(^)())block  queue:(dispatch_queue_t)queue afterDelay:(NSTimeInterval)delay;
 
 /**
  Executes a given block after a specificed delay, with a weak reference to the calling object.
--- Annex/NSObject+Annex.m
+++ Annex/NSObject+Annex.m
@@ -11,10 +11,10 @@
 
 @implementation NSObject (Annex)
 
-- (void)executeBlock:(void(^)(__weak id this))block withCallback:(void(^)(__weak id this))callback
+- (void)executeBlock:(void(^)(__weak id this))block queue:(dispatch_queue_t)queue withCallback:(void(^)(__weak id this))callback
 {
     __weak typeof(self) weakSelf = self;
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+    dispatch_async(queue, ^{
         
         block(weakSelf);
         
@@ -24,37 +24,59 @@
     });
 }
 
-+ (void)executeBlock:(void(^)())block withCallback:(void(^)())callback
++ (void)executeBlock:(void(^)())block queue:(dispatch_queue_t)queue withCallback:(void(^)())callback
 {
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+    dispatch_async(queue, ^{
         
         block();
         
         dispatch_sync(dispatch_get_main_queue(), ^{
             callback();
         });
-    });    
+    });
+    
 }
 
-- (void)executeBlock:(void(^)(__weak id this))block afterDelay:(NSTimeInterval)delay
+- (void)executeBlock:(void(^)(__weak id this))block withCallback:(void(^)(__weak id this))callback
+{
+    [self executeBlock:block queue:dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0) withCallback:callback];
+}
+
++ (void)executeBlock:(void(^)())block withCallback:(void(^)())callback
+{
+    [[self class] executeBlock:block queue:dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0) withCallback:callback];
+}
+
+- (void)executeBlock:(void(^)(__weak id this))block  queue:(dispatch_queue_t)queue afterDelay:(NSTimeInterval)delay
 {
     __weak typeof(self) weakSelf    = self;
     int64_t delta                   = (int64_t)(1.0e9 * delay);
     dispatch_time_t popTime         = dispatch_time(DISPATCH_TIME_NOW, delta);
     
-    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+    dispatch_after(popTime, queue, ^(void){
         block(weakSelf);
     });
+    
 }
 
-+ (void)executeBlock:(void(^)())block afterDelay:(NSTimeInterval)delay
++ (void)executeBlock:(void(^)())block  queue:(dispatch_queue_t)queue afterDelay:(NSTimeInterval)delay
 {
     int64_t delta           = (int64_t)(1.0e9 * delay);
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delta);
     
-    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+    dispatch_after(popTime, queue, ^(void){
         block();
     });
+}
+
+- (void)executeBlock:(void(^)(__weak id this))block afterDelay:(NSTimeInterval)delay
+{
+    [self executeBlock:block queue:dispatch_get_main_queue() afterDelay:delay];
+}
+
++ (void)executeBlock:(void(^)())block afterDelay:(NSTimeInterval)delay
+{
+    [[self class] executeBlock:block queue:dispatch_get_main_queue() afterDelay:delay];
 }
 
 @end
