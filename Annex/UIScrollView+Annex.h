4953ea9f4cd6646f2a7c4161d44f4f11e5cf3f6c
Wess Cope <wcope@me.com>
2014-04-09 11:33:42 -0400

    Adds category to UIScrollView to allow for a floating header view that's visiblity depends on contentOffset

---
+++ Annex/UIScrollView+Annex.h
@@ -0,0 +1,13 @@
+//
+//  UIScrollView+Annex.h
+//  Annex
+//
+//  Created by Wess Cope on 4/9/14.
+//  Copyright (c) 2014 Wess Cope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScrollView (Annex)
+@property (strong, nonatomic) UIView *floatingHeaderView;
+@end
